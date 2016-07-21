b086f24c461b4d347aaa98b3db23fcf30bd82d73
delicast <delicast@gmail.com>
2016-07-21 16:09:09 +0100

    migration add admin

---
+++ database/migrations/2016_07_21_145736_Add_Admin_User.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddAdminUser extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        DB::table('level')->insert([array(
+            'id' => 1,
+            'name' => 'David Elizalde',
+            'email'=> 'delicast@gmail.com',
+            'password'=> '$2y$10$GqEl8iB2wy8SfemLTasJyO.T2JHFqtMIBeGlXhtlES7qYz/yLgAHC',
+            'admin'=> '1',
+            'remember_token'=>'OPEa26wqpXQy1WlnqGRfwStX6ESEiOlvLQTI5Aec1Xtt8hdkldGzOWf9uzzO',
+            'created_at' => '2016-07-21 15:55:48',
+            'updated_at' => '2016-07-21 15:55:48',
+            'type'=> '0',
+            
+        )]);
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            //
+        });
+    }
+}
