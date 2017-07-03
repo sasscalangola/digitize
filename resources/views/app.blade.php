e78abafe6c3bc544a7911c610712dc7f26c96957
David Elizalde <delicast@gmail.com>
2017-07-03 18:55:28 +0100

    copyright

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -27,7 +27,7 @@
         <span class="glyphicon glyphicon-chevron-up"></span>
     </a>
     <p>TEST VERSION - NOT FULLY FUNCTIONAL </p>
-
+    <p>(c) SASSCAL </p>
     <img src="/img/SASSCAL_LOGO_colour_ANGOLA.png" width="300">
 </footer>
 
